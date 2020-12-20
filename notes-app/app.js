3362c2dc132ec2ae6d33a9cbb2a2c3ce3261e24f
Tong Bao Loc <loc.tong@ptnglobalcorp.com>
2020-12-20 13:07:20 +0700

    20/12/202

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,16 @@
 // TODO how to use import
 //import validator from 'validator';
+// import from own files
 const add1 = require('./utils');
+
 const getNotes = require('./notes');
+// import from npm
 const validator = require('validator');
 const chalk = require('chalk');
+// import from core module
+const fileSystem = require('fs');
+
+const yargs = require('yargs');
 
 // const fs = require('fs');
 
@@ -15,3 +22,23 @@ console.log(chalk.bgRed(getNotes()))
 
 console.log(validator.isEmail('sdsd@@sdss2d.vn'))
 
+yargs.command({
+    command: 'add',
+    describe : 'Add ',
+    builder: {
+        title: {
+            describe: 'Note ',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (){
+        console.log('add commands')
+    }
+})
+
+// 20/12/2020
+// const params = process.argv;
+// console.log(params)
+// yargs.version('1.1.0')
+console.log(yargs.argv)
