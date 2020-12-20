bbdb55b05e1e5a3b1b4c2f2383b0942e512c7b47
Tong Bao Loc <loc.tong@ptnglobalcorp.com>
2020-12-20 22:45:45 +0700

    20/12/202 Add remove command

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,30 +7,46 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'list',
     describe: 'List',
-    handler: function (argv){
+    handler: function (argv) {
         console.log(noteApi.loadAll());
     }
-    
+
 })
 
 // Create add command
 yargs.command({
     command: 'add',
-    describe : 'Add ',
+    describe: 'Add ',
     builder: {
         title: {
             describe: 'Note ',
             demandOption: true,
             type: 'string'
-        }, 
+        },
         body: {
             describe: 'Body',
             demandOption: true,
             type: 'string'
         }
     },
-    handler: function (argv){
+    handler: function (argv) {
         noteApi.add(argv.title, argv.body);
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: '',
+    builder: {
+        title: {
+            describe: 'Note ',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        noteApi.remove(argv.title)
     }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,42 +1,54 @@
 const fileStream = require('fs')
-
+const chalk = require('chalk')
 const notesDb = 'notes_db.json'
 
-const add = function(title, body) {
+const add = function (title, body) {
     const datas = loadAll();
 
     if (checkDuplicate(title, datas)) {
-        console.log('title is taken');
+        console.log(chalk.green.inverse('title is taken'))
     } else {
         datas.push({
-            title : title,
-            body : body
+            title: title,
+            body: body
         })
 
         save(datas)
     }
 }
 
-const checkDuplicate = function(title, datas) {
+const checkDuplicate = function (title, datas) {
     // if no value , undefined is returned
-    return datas.find( x => x.title == title);
+    return datas.find(x => x.title == title);
 }
 
-const save = function(datas) {
+const save = function (datas) {
     fileStream.writeFileSync(notesDb, JSON.stringify(datas))
 }
 
-const loadAll = function() {
+const loadAll = function () {
     try {
         return JSON.parse(fileStream.readFileSync(notesDb))
-    } catch (e){
+    } catch (e) {
         return [];
     }
 }
 
+const remove = function (title) {
+    const datas = loadAll();
+
+    if (checkDuplicate(title, datas)) {
+        const nodeToKeep = datas.filter(x => x.title !== title);
+        save(nodeToKeep)
+    } else {
+        console.log(chalk.red.green('no matching data found'));
+
+    }
+}
 
 module.exports = {
     add: add,
-    loadAll: loadAll
+    loadAll: loadAll,
+    remove: remove
 }
 
